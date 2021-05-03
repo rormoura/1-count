5d59e9a7cfd833ffb961bc7e114b661a898744bf
rormoura <rrm2@cin.ufpe.br>
2021-05-03 13:36:11 -0300

    Count()

--- main.c
+++ main.c
@@ -8,16 +8,9 @@ struct integer // nodes from the linked list
 };
 typedef struct integer Int;
 
-struct list // linked list
+void add (Int **head, int a) // function to add a new node to the list
 {
-    Int *first;
-    Int *last;
-};
-typedef struct list List;
-
-void add(List list1, int a) // function to add a new node to the list
-{
-
+    int i = 0;
     Int *aux = (Int *) malloc(sizeof(Int)); // to receive the integer
     Int *aux1 = (Int *) malloc(sizeof(Int)); // to iterate down the list
 
@@ -30,37 +23,33 @@ void add(List list1, int a) // function to add a new node to the list
     aux->n = a; // filling in the node that will be added
     aux->next = NULL;
 
-    if (list1.first == NULL) // checking if the list is empty
+    if (*head == NULL) // checking if the list is empty
     {
-
-        list1.first = aux; // adding the new node to the first position of the list
-
+        *head = aux; // adding the new node to the first position of the list
     }
     else
     {
 
-        aux1 = list1.first; // iterating down the list
-        while (aux1->next != NULL)
+        aux1 = *head; // iterating down the list
+        while (aux1->next != NULL) // stops when reaches the last node
         {
             aux1 = aux1->next;
         }
 
         aux1->next = aux; // adding the new node to the list
-        list1.last = aux;
-
     }    
 
     printf("New node added\n"); // success message 
 
-    i = 1;
-    aux1 = list1.first; // printing the new list
-    while (aux1->next != NULL)
+    i = 1; // printing the new list
+    aux1 = *head; 
+    while (aux1->next != NULL) // stops when reaches the last node
     {
 
         printf("Integer %d: %d\n", i, aux1->n);
         aux1 = aux1->next;
         i++;
-        if (aux1->next ++NULL)
+        if (aux1->next == NULL)
         {
 
             printf("Integer %d: %d\n", i, aux1->n);
@@ -68,26 +57,64 @@ void add(List list1, int a) // function to add a new node to the list
         }
 
     }
+    
+}
+
+void count (Int *head, int number)
+{
+    int counter = 0; // we need a counter, like, obviously
+    Int *aux = (Int *) malloc(sizeof(Int)); // allocating the aux that we're gonna use to iterate down the list
+
+    aux = head;
+
+    while (aux->next != NULL) // iterating down the list to see how many times the given number occurs
+    {
+        if(aux->n == number)
+        {
+            counter++;
+        }
+        aux = aux->next;
+        if(aux->next == NULL)
+        {
+            if(aux->n == number)
+            {
+            counter++;
+            }
+        }
+    }
+
+    printf("%d appears %d times in the list\n", number, counter);
 
     free(aux); // realising the used memory
-    free(aux1); 
 }
 
 int main()
 {
     int i = 0; // to iteration
     int b = 0; // to adding new nodes
+    int number = 0;
 
-    List list1; // creating the linked list we're gonna use
-    list1.first = NULL;
-    list1.last = NULL;
+    Int *head = (Int *) malloc(sizeof(Int)); // creating the linked list head we're gonna use
+    if(head == NULL)
+    {
+        printf("Allocation error.\n");
+        exit(-1);
+    }
+    head = NULL;
 
     for (i = 0; i < 6; i++) // for loop to add the five nodes of the list
     {
 
         printf("Input the integer: ");
         scanf("%d", &b);
-        add(list1, b);
+        add(&head, b);
 
     }
+
+    printf("Input the number that u wanna know how many times occurs: ");
+    scanf("%d", &number);
+
+    count(head, number); // calling the count() function
+
+    return 0;
 }
